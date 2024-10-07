89a9b45a018d507a12ef72e443c60614d5d467f1
a23xacobobc <a23xacobobc@iessanclemente.net>
2024-10-07 12:56:05 +0200

    Primero

--- UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
+++ UD01_2_AnimalList/app/src/main/java/com/example/ud01_2_animallist/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.ud01_2_animallist
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        btnSend.setOnClickListener{
+            val spinnerAnimalType = findViewById<Spinner>(R.id.AnimalList)
+            val textKindsAnimals = findViewById<TextView>(R.id.textKindsAnimals)
+
+            textKindsAnimals.text = getAnimals(spinnerAnimalType.selectedItemId).joinToString("\n")
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun getAnimals(id : Long) = when (id){
+        0L -> listOf(R.string.Sheppard, R.string.chihuahua)
+        1L -> listOf(R.string.Egypt, R.string.Persian)
+        2L -> listOf(R.string.Mallard, R.string.White_Call)
+        else  -> listOf()
     }
 }
---
+++ UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/MessageFragment.kt
@@ -0,0 +1,27 @@
+package com.example.ud03_1_fragmentsencrypt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+
+class MessageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val btnNext = view.findViewById<Button>(R.id.btn_next)
+        btnNext.setOnClickListener{
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+        return view
+    }
+}
